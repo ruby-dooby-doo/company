ff4cb8a8bd992b149d863a0debd6d2b439f334aa
Brian <riceb53@gmail.com>
2018-06-19 18:48:23 -0500

    add options hash for employee class

--- employee.rb
+++ employee.rb
@@ -26,11 +26,16 @@ class Employee
   attr_reader :first_name, :last_name, :active, :salary
   attr_writer :first_name
 
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    # {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => true}
+    # how can i get the first_name from input_options?
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+    # @last_name = input_last_name
+    # @salary = input_salary
+    # @active = input_active
   end
 
   def print_info
@@ -42,13 +47,24 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Danilo", "Campos", 70000, true)
-employee2 = Employee.new("Majora", "Carter", 80000, false)
+# explicit options hash
+employee1 = Employee.new({:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => true})
 
-employee1.print_info
+# 'fancy'
+employee2 = Employee.new(first_name: "Majora", last_name: "Carter", salary: 80000, active: false)
+
+# employee1.print_info
 # employee1.give_annual_raise
 # employee1.print_info
 
-p employee1.first_name
-employee1.first_name = 'billybob'
-p employee1.first_name
+# p employee1.first_name
+# employee1.first_name = 'billybob'
+# p employee1.first_name
+
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+
+p employee2.first_name
+employee2.first_name = 'billybob'
+p employee2.first_name
