2851a266b9dfd4df67ff8fb7d8423301a8c8b217
Brian <riceb53@gmail.com>
2018-06-18 20:14:35 -0500

    replace string keys with value keys in employees hashes

--- employee.rb
+++ employee.rb
@@ -12,10 +12,10 @@
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
 
 # hash
-employee1 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
+employee1 = {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => true}
 
-employee2 = {"last_name" => "Carter", "salary" => 80000, "active" => false, "first_name" => "Majora"}
+employee2 = {last_name: "Carter", salary: 80000, active: false, first_name: "Majora"}
 
-p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
-p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year."
+p "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]} per year."
+p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
