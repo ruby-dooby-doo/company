98ca077d3b79b4a89ac6ce4bb10f783165e5d532
Brian <riceb53@gmail.com>
2018-06-18 21:19:28 -0500

    add annual raise, add getter and setter methods, modify print_info to use getter methods

--- employee.rb
+++ employee.rb
@@ -12,10 +12,60 @@
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
 
 # hash
-employee1 = {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => true}
+# employee1 = {:first_name => "Danilo", :last_name => "Campos", :salary => 70000, :active => true}
 
-employee2 = {last_name: "Carter", salary: 80000, active: false, first_name: "Majora"}
+# employee2 = {last_name: "Carter", salary: 80000, active: false, first_name: "Majora"}
 
-p "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]} per year."
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# p "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]} per year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
+
+# class
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def first_name=(input_first_name)
+    @first_name = input_first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def salary
+    @salary
+  end
+
+  def active
+    @active
+  end
+
+  def print_info
+    p "#{first_name} #{last_name} makes $#{salary} per year."
+  end
+
+  def give_annual_raise
+    @salary *= 1.05
+  end
+end
+
+employee1 = Employee.new("Danilo", "Campos", 70000, true)
+employee2 = Employee.new("Majora", "Carter", 80000, false)
+
+employee1.print_info
+employee1.give_annual_raise
+employee1.print_info
+
+# p employee1.first_name
+# employee1.first_name = 'billybob'
+# p employee1.first_name
