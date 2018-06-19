b1d5e17e9ea1e578ab894e38b2c5dd5e5d945939
Brian <riceb53@gmail.com>
2018-06-18 19:52:35 -0500

    model employee using hash

--- employee.rb
+++ employee.rb
@@ -2,11 +2,20 @@
 # keep track of fn, ln, active status, salary
 
 # array
-employee1 = ["Danilo", "Campos", 70000, true]
-employee2 = ["Majora", "Carter", 80000, false]
+# employee1 = ["Danilo", "Campos", 70000, true]
+# employee2 = ["Majora", "Carter", 80000, false]
 
-# concatenation
-p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
+# # concatenation
+# p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
 
-# interpolation
-p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+# # interpolation
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+
+# hash
+employee1 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
+
+employee2 = {"last_name" => "Carter", "salary" => 80000, "active" => false, "first_name" => "Majora"}
+
+p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year."
+
