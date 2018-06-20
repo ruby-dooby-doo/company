0ef54a07b536e4952b06b7f328a9059eb933dfec
Brian <riceb53@gmail.com>
2018-06-19 20:18:59 -0500

    DRY up bike and car classes with Transportation class

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Transportation
   def initialize
     @speed = 0
     @direction = 'north'
@@ -15,31 +15,27 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Transportation
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Transportation
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+
+# bike1 = Bike.new
+# bike1.accelerate
+# bike1.brake
+# bike1.ring_bell
+
+# car1 = Car.new
+# car1.accelerate
+# car1.brake
+# car1.honk_horn
